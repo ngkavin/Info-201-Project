97d89949f17d8531899f288eb31094ec533a391a
Kavin <ng.kavin97@gmail.com>
2018-11-29 17:22:41 -0800

    read grad set

--- scripts.R
+++ scripts.R
@@ -13,9 +13,12 @@ library(RCurl)
 library(XML)
 
 # Global variables and functions are defined here to reduce clutter in server.R
+
 all_ages <- read.csv("data/all-ages.csv", stringsAsFactors = FALSE)
+grad <- read.csv("data/grad-students.csv", stringsAsFactors = FALSE)
+
 # Entries are given in all caps. This will change the major names to be in "title case"
-majors_list <- sapply(tolower(all_ages$Major), tools::toTitleCase)
+majors_list <- sapply(tolower(grad$Major), tools::toTitleCase)
 majors_list <- as.data.frame(majors_list, stringsAsFactors = FALSE)$majors_list
 # Function picks a major from the majors_list at random and returns it.
 get_random_major <- function() {majors_list[sample(1:length(majors_list), 1)]}
