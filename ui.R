86e135638388f3c179029481cc6bccbbc3cafad5
Kavin <ng.kavin97@gmail.com>
2018-12-01 11:43:09 -0800

    fix sidebar

--- ui.R
+++ ui.R
@@ -7,12 +7,12 @@ source("text.R")
 shinyUI(fluidPage(
   theme = "bootstrap.css",
   tags$br(),
-  tags$h1(class="display-4", "Exploring College Majors"),
+  tags$h1(class = "display-4", "Exploring College Majors"),
   tags$br(),
 
   sidebarLayout(
     conditionalPanel(
-      condition="input.tabs != 'Welcome!'",
+      condition = "input.tabs != 'Welcome!'",
       sidebarPanel(
         width = 4,
         selectizeInput("select",
@@ -20,25 +20,29 @@ shinyUI(fluidPage(
           choices = NULL,
           options = list(maxItems = 1, maxOptions = 5, items = NULL, openOnFocus = FALSE)
         ),
-        actionButton("randomize", label = "Randomize")
-        ),
-      conditionalPanel(
-        HTML("<br>"),
-        condition = "input.tabs == 'Popularity and Wages'",
-        selectInput("status",
-                    label = "Select Graduate Status",
-                    choices = list("Graduate" = "Grad", "Undergraduate" = "Nongrad"),
-                    selected = "Nongrad"),
-        selectInput("percent",
-                    label = "Select Wage Distribution",
-                    choices = list("25th Percentile" = "P25", 
-                                   "50th Percentile" = "median", 
-                                   "75th Percentile" = "P75"),
-                    selected = "median"),
-        actionButton("toggle", label = "Toggle Legend")
+        actionButton("randomize", label = "Randomize"),
+        conditionalPanel(
+          HTML("<br>"),
+          condition = "input.tabs == 'Popularity and Wages'",
+          selectInput("status",
+            label = "Select Graduate Status",
+            choices = list("Graduate" = "Grad", "Undergraduate" = "Nongrad"),
+            selected = "Nongrad"
+          ),
+          selectInput("percent",
+            label = "Select Wage Distribution",
+            choices = list(
+              "25th Percentile" = "P25",
+              "50th Percentile" = "median",
+              "75th Percentile" = "P75"
+            ),
+            selected = "median"
+          ),
+          actionButton("toggle", label = "Toggle Legend")
+        )
       )
     ),
-  
+
     mainPanel(
       tabsetPanel(
         id = "tabs",
