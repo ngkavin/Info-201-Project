faa846806d7b9455818b90fbc13a6abe0af870ee
carmelitadeleon <carmelitadeleon1994@gmail.com>
2018-12-01 13:35:20 -0800

    Fix summary

--- text.R
+++ text.R
@@ -5,10 +5,11 @@ summary <- "This is a preview of what our project is about as it contains:
 
 The word cloud displayed shows all the majors based on their popularity. 
 
-Three questions that our project will answer are:
-1) What is the male:female ratio in popular majors?
-2) How does gender affect the salary of individuals with the same major?
-3) What are the employement statistics for a given major?"
+<br>Three questions that our project will answer are:
+<ol>1) What is the male:female ratio in popular majors?</ol>
+<ol>2) How does gender affect the salary of individuals with the same major?</ol>
+<ol>3) What are the employment statistics for a given major?</ol>"
+
 
 # Employment
 
