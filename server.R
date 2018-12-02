9d87985543d8b41f5abf36521b69dea603bc3bbd
carmelitadeleon <carmelitadeleon1994@gmail.com>
2018-12-01 22:05:37 -0800

    Change size of text

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ shinyServer(function(input, output, session) {
 
   # Displays a word cloud of majors based on popularity
   output$word_cloud2 <- renderWordcloud2({
-    all_majors <- wordcloud2(all_majors, size = .45, shape = 'circle', color = c(
+    all_majors <- wordcloud2(all_majors, size = .4, shape = 'circle', color = c(
       "#64EDD9",
       "#5FE6CD",
       "#5AE0C1",
