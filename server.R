024383a16938081cc57e6c8e30e229e3e88d0159
carmelitadeleon <carmelitadeleon1994@gmail.com>
2018-12-01 21:51:03 -0800

    Change colors of word cloud

--- server.R
+++ server.R
@@ -10,7 +10,26 @@ shinyServer(function(input, output, session) {
 
   # Displays a word cloud of majors based on popularity
   output$word_cloud2 <- renderWordcloud2({
-    all_majors <- wordcloud2(all_majors, size = .4, shape = 'circle')
+
+    all_majors <- wordcloud2(all_majors, size = .4, color = c(
+      "#64EDD9",
+      "#5FE6CD",
+      "#5AE0C1",
+      "#55D9B5",
+      "#50D3A9",
+      "#4BCC9D",
+      "#46C691",
+      "#41BF85",
+      "#3CB979",
+      "#37B36D",
+      "#32AC61",
+      "#2DA655",
+      "#289F49",
+      "#1E9231",
+      "#198C25",
+      "#15861A"
+    ))
+    
     all_majors
   })
 
