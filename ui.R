ea2804fcc3ca0a61dbe45a3e859d15e46d1106a6
Kavin Nguyen <ng.kavin97@gmail.com>
2018-12-02 17:30:50 -0800

    add space

--- ui.R
+++ ui.R
@@ -53,7 +53,7 @@ shinyUI(fluidPage(
         type = "tabs",
         tabPanel("Welcome!", wordcloud2Output("word_cloud2", width = "900", height = "500" )),
         tabPanel("Employment", plotlyOutput("employment_chart")),
-        tabPanel("Gender", plotlyOutput("gender_chart"), HTML("<p>", gender, "</p>")),
+        tabPanel("Gender", tags$br(), plotlyOutput("gender_chart"), HTML("<p>", gender, "</p>")),
         tabPanel("Popularity and Wages", tags$br(), plotlyOutput("popularity_plot"), tags$br(), HTML("<p>", pop1, "</p>"))
       )
     )
